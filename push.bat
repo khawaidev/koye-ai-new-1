b932935fb158b1e8db7a2fc91c94612405b851f3
khawaidev <khawaidev@gmail.com>
2026-03-16 17:05:56 +0530

    "push"

---
+++ push.bat
@@ -0,0 +1,7 @@
+@echo off
+git init
+git remote add origin "https://github.com/khawaidev/koye-ai-systems2.0.git"
+git add .
+git commit -m "Initial push"
+git branch -M main
+git push -u origin main
